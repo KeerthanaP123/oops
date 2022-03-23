635837fa4e32aa69ed7e05cbe64cb04611bcf688
KeerthanaP123 <97934590+KeerthanaP123@users.noreply.github.com>
2022-03-23 11:46:37 +0530

    Add files via upload

---
+++ Commandtwo.java
@@ -0,0 +1,9 @@
+public class Commandtwo{
+	public static void main(String args[]){
+		for(String a:args)
+		{
+			System.out.println(a);
+			
+		}
+	}
+}
---
+++ Operations.java
@@ -0,0 +1,12 @@
+public class Operations{
+	public static void main(String args[]){
+		int a=5,b=4;
+		System.out.println("First number="+a);
+		System.out.println("Second number="+b);
+		System.out.println("The sum is="+(a+b));
+		System.out.println("The difference="+(a-b));
+		System.out.println("The product="+(a*b));
+		System.out.println("The quotient="+(a/b));
+		System.out.println("The modulus="+(a%b));
+	}
+}
